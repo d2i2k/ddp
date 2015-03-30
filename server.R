97f8e8171ef31a357c9d9ee6204cca96d3743488
Douglas ("Doug") Okamoto <d2i2k@aol.com>
2015-03-30 16:38:57 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,43 +1,31 @@
-#setwd("C:/COURSERA/Development Data Products/project/mpg")
-library(shiny)
-library(datasets)
+library(HistData)
+data(GaltonFamilies)
+library(ggplot2)
 
-mtcarsData <- mtcars
-mtcarsData$am <- factor(mtcarsData$am, labels = c("Automatic", "Manual"))
+fit <- lm(childHeight ~ midparentHeight,data=GaltonFamilies)
+fit1 <- lm(childHeight ~ midparentHeight,data=subset(GaltonFamilies,gender=="female"))
+fit2 <- lm(childHeight ~ midparentHeight,data=subset(GaltonFamilies,gender=="male"))
+fit3 <- lm(childHeight ~ midparentHeight+gender,data=GaltonFamilies)
 
-shinyServer(function(input, output) {
-        
-        formulaText <- reactive({
-                paste("mpg ~", input$variable)
-        })
-        
-        formulaTextPoint <- reactive({
-                paste("mpg ~", "as.integer(", input$variable, ")")
-        })
-        
-        fit <- reactive({
-                lm(as.formula(formulaTextPoint()), data=mtcarsData)
-        })
-        
-        output$caption <- renderText({
-                formulaText()
-        })
-        output$mpgBoxPlot <- renderPlot({
-                boxplot(as.formula(formulaText()), 
-                        data = mtcarsData, ylab = "Miles per gallon",
-                        main = paste("Miles per gallon vs.", "variable (", input$variable, ")"),
-                        outline = input$outliers)
-        })
-        
-        output$fit <- renderPrint({
-                summary(fit())
-        })
-        
-        output$mpgPlot <- renderPlot({
-                with(mtcarsData, {
-                        plot(as.formula(formulaTextPoint()))
-                        abline(fit(), col=2)
-                })
-        })
-        
-})
+summary(fit)
+summary(fit1)
+summary(fit2)
+summary(fit3)
+
+shinyServer(
+  function(input, output) {
+    output$scatterPlot <- renderPlot({
+      plot(jitter(GaltonFamilies$childHeight) ~ GaltonFamilies$midparentHeight,xlab="Average Height of the Parents (in inches)",ylab="Height of the Child (in inches)",main="Scatterplot of Galton Family Data on 934 Adult Children (481 sons, 453 daughters)",pch=19,frame.plot=FALSE,
+           col=ifelse(GaltonFamilies$gender=="female", "pink", "light blue"))
+      legend(65,80,pch=c(19,19),col=c("pink","light blue"),c("female", "male"),bty="o",cex=.8)
+      lines(GaltonFamilies$midparentHeight,fitted(fit),col="magenta",lwd=2)
+      abline(h=mean(GaltonFamilies$childHeight),lwd=1)
+      mean <- input$mean
+      lines(c(mean,mean),c(0,200),lwd=3)
+    })
+    output$scatterplotMatrix <- renderPlot({
+      qplot(midparentHeight,childHeight,colour=gender,data=GaltonFamilies,xlab="Average Height of the Parents (in inches)",ylab="Height of the Child (in inches)",main="Scatterplot matrix of Galton Family Data by Gender of the Child",facets=.~gender,geom=c("point","smooth"),method="lm")
+      })
+  }
+)
+
