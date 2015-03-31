1247e6a96480c1fcd075b7cb318cd16e4055ca58
Douglas ("Doug") Okamoto <d2i2k@aol.com>
2015-03-31 01:10:14 -0700

    Update server.R

--- server.R
+++ server.R
@@ -15,16 +15,21 @@ summary(fit3)
 shinyServer(
   function(input, output) {
     output$scatterPlot <- renderPlot({
-      plot(jitter(GaltonFamilies$childHeight) ~ GaltonFamilies$midparentHeight,xlab="Average Height of the Parents (in inches)",ylab="Height of the Child (in inches)",main="Scatterplot of Galton Family Data on 934 Adult Children (481 sons, 453 daughters)",pch=19,frame.plot=FALSE,
-           col=ifelse(GaltonFamilies$gender=="female", "pink", "light blue"))
-      legend(65,80,pch=c(19,19),col=c("pink","light blue"),c("female", "male"),bty="o",cex=.8)
-      lines(GaltonFamilies$midparentHeight,fitted(fit),col="magenta",lwd=2)
-      abline(h=mean(GaltonFamilies$childHeight),lwd=1)
-      mean <- input$mean
-      lines(c(mean,mean),c(0,200),lwd=3)
+    	plot(jitter(GaltonFamilies$childHeight) ~ GaltonFamilies$midparentHeight,xlab="Average Height of the Parents (in inches)",ylab="Height of the Child (in inches)",main="Figure 1. Scatterplot of Galton Family Data with Fitted Values",pch=19,frame.plot=FALSE, col=ifelse(GaltonFamilies$gender=="female", "pink", "light blue"))
+           	legend(65,80,pch=c(19,19),col=c("pink","light blue"),c("female", "male"),bty="o",cex=.8)
+      	lines(GaltonFamilies$midparentHeight,fitted(fit),col="magenta",lwd=2)
+      	abline(h=mean(GaltonFamilies$childHeight),lwd=1)
+      	mean <- input$mean
+      	lines(c(mean,mean),c(0,200),lwd=3)
     })
     output$scatterplotMatrix <- renderPlot({
-      qplot(midparentHeight,childHeight,colour=gender,data=GaltonFamilies,xlab="Average Height of the Parents (in inches)",ylab="Height of the Child (in inches)",main="Scatterplot matrix of Galton Family Data by Gender of the Child",facets=.~gender,geom=c("point","smooth"),method="lm")
+     	qplot(midparentHeight,childHeight,colour=gender,data=GaltonFamilies,xlab="Average Height of the Parents (in inches)",ylab="Height of the Child (in inches)",main="Scatterplot matrix of Galton Family Data by Gender of the Child",facets=.~gender,geom=c("point","smooth"),method="lm")
+      })
+   output$scatterPlot2 <- renderPlot({
+	plot(jitter(GaltonFamilies$childHeight) ~ GaltonFamilies$midparentHeight,xlab="Average Height of the Parents (in inches)",ylab="Height of the Child (in inches)",main="Figure 3. Scatterplot of Galton Family Data with Fitted Values by Gender ",pch=19,frame.plot=FALSE, col=ifelse(GaltonFamilies$gender=="female", "pink", "light blue"))
+           	legend(65,80,pch=c(19,19),col=c("pink","light blue"),c("female", "male"),bty="o",cex=.8)
+	lines(GaltonFamilies$midparentHeight,coefficients(fit3)*GaltonFamilies$midparentHeight 	col="pink",lwd=2)
+	lines(GaltonFamilies$midparentHeight,coefficients(fit3)*GaltonFamilies$midparentHeight 	col="light blue",lwd=2)
       })
   }
 )
